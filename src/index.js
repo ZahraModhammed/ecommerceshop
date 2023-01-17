4412ab0f1995a509425cd3def577d12afb87d9bd
HsoubAcademy <academy@hsoub.co>
2023-01-17 16:27:58 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+import './css/style.css';
+import 'jquery/dist/jquery';
+import 'popper.js/dist/popper';
+import 'bootstrap/dist/js/bootstrap';
+import '@fortawesome/fontawesome-free/js/all.min';
+
+$(document).ready(function() {
+ $('[data-toggle="tooltip"]').tooltip();
+});
